2ef40ab576e34aa423b334e53bb7f2540f2deed5
Danisaurio12 <danieljscome2015@gmail.com>
2026-09-02 22:44:00 -0600

    Estructura base con Bootstrap: Inicio y Clientes

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+USUARIOS = {
+    "admin": "1234",
+    "daniel": "flask2026"
+}
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/clientes', methods=['GET', 'POST'])
+def clientes():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nit = request.form['nit']
+        correo = request.form['correo']
+        telefono = request.form['telefono']
+        direccion = request.form['direccion']
+        return render_template('clientes_confirmacion.html',
+                                nombre=nombre, nit=nit, correo=correo,
+                                telefono=telefono, direccion=direccion)
+    else:
+        return render_template('clientes.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
